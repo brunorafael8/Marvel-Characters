e011430aa4d65359e286de0c1056b547d4a54852
brunorafael8 <bruninho_lito@hotmail.com>
2019-06-04 18:49:23 -0300

    (add) route on App src

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react'
+import { createAppContainer, createStackNavigator } from 'react-navigation'
+import CharacterDetail from './screens/CharacterDetail'
 import Home from './screens/Home'
 
-const App = () => {
-  return <Home />
-}
+const App = createAppContainer(
+  createStackNavigator(
+    {
+      Home: {
+        screen: Home,
+        navigationOptions: {
+          header: null,
+        },
+      },
+      Character_detail: {
+        screen: CharacterDetail,
+        navigationOptions: {
+          header: null,
+        },
+      },
+    },
+    {
+      initialRouteName: 'Home',
+    },
+  ),
+)
 
 export default App
