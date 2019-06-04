9be0d485774b473fde49c60e0bc551071bb0f7b0
brunorafael8 <bruninho_lito@hotmail.com>
2019-06-04 13:56:38 -0300

    (add) home screen on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,8 @@
 import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
-import { createFragmentContainer, graphql, QueryRenderer } from 'react-relay'
-import environment from './RelayEnvironment'
+import Home from './screens/Home'
 
-const App = props => {
-  const { characters } = props.query
-  return (
-    <View style={styles.container}>
-      {characters.map(char => (
-        <Text key={char.id}>{char.name}</Text>
-      ))}
-    </View>
-  )
+const App = () => {
+  return <Home />
 }
 
-const query = graphql`
-  query AppQuery {
-    characters(where: { series: 24229 }, orderBy: modified_asc) {
-      id
-      name
-      description
-      thumbnail
-    }
-  }
-`
-
-const AppRenderer = () => {
-  return (
-    <QueryRenderer
-      environment={environment}
-      query={query}
-      render={({ error, props }) => {
-        if (error) {
-          console.error(new Error(error))
-        }
-
-        if (props) {
-          return <App query={props} />
-        } else {
-          return <Text>loading...</Text>
-        }
-      }}
-    />
-  )
-}
-export default AppRenderer
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-})
+export default App
--- src/screens/Home/__generated__/HomeQuery.graphql.ts
+++ src/screens/Home/__generated__/HomeQuery.graphql.ts
@@ -1,21 +1,19 @@
 /* tslint:disable */
 
-import { ConcreteRequest } from "relay-runtime";
-export type HomeQueryVariables = {};
+import { ConcreteRequest } from 'relay-runtime'
+export type HomeQueryVariables = {}
 export type HomeQueryResponse = {
-    readonly characters: ReadonlyArray<{
-        readonly id: string | null;
-        readonly name: string | null;
-        readonly description: string | null;
-        readonly thumbnail: string | null;
-    }> | null;
-};
+  readonly characters: ReadonlyArray<{
+    readonly id: string | null
+    readonly name: string | null
+    readonly description: string | null
+    readonly thumbnail: string | null
+  }> | null
+}
 export type HomeQuery = {
-    readonly response: HomeQueryResponse;
-    readonly variables: HomeQueryVariables;
-};
-
-
+  readonly response: HomeQueryResponse
+  readonly variables: HomeQueryVariables
+}
 
 /*
 query HomeQuery {
@@ -28,85 +26,86 @@ query HomeQuery {
 }
 */
 
-const node: ConcreteRequest = (function(){
-var v0 = [
-  {
-    "kind": "LinkedField",
-    "alias": null,
-    "name": "characters",
-    "storageKey": "characters(orderBy:\"modified_asc\",where:{\"series\":24229})",
-    "args": [
-      {
-        "kind": "Literal",
-        "name": "orderBy",
-        "value": "modified_asc"
-      },
-      {
-        "kind": "Literal",
-        "name": "where",
-        "value": {
-          "series": 24229
-        }
-      }
-    ],
-    "concreteType": "Character",
-    "plural": true,
-    "selections": [
-      {
-        "kind": "ScalarField",
-        "alias": null,
-        "name": "id",
-        "args": null,
-        "storageKey": null
-      },
-      {
-        "kind": "ScalarField",
-        "alias": null,
-        "name": "name",
-        "args": null,
-        "storageKey": null
-      },
-      {
-        "kind": "ScalarField",
-        "alias": null,
-        "name": "description",
-        "args": null,
-        "storageKey": null
-      },
-      {
-        "kind": "ScalarField",
-        "alias": null,
-        "name": "thumbnail",
-        "args": null,
-        "storageKey": null
-      }
-    ]
+const node: ConcreteRequest = (function() {
+  var v0 = [
+    {
+      kind: 'LinkedField',
+      alias: null,
+      name: 'characters',
+      storageKey: 'characters(orderBy:"modified_asc",where:{"series":24229})',
+      args: [
+        {
+          kind: 'Literal',
+          name: 'orderBy',
+          value: 'modified_asc',
+        },
+        {
+          kind: 'Literal',
+          name: 'where',
+          value: {
+            series: 24229,
+          },
+        },
+      ],
+      concreteType: 'Character',
+      plural: true,
+      selections: [
+        {
+          kind: 'ScalarField',
+          alias: null,
+          name: 'id',
+          args: null,
+          storageKey: null,
+        },
+        {
+          kind: 'ScalarField',
+          alias: null,
+          name: 'name',
+          args: null,
+          storageKey: null,
+        },
+        {
+          kind: 'ScalarField',
+          alias: null,
+          name: 'description',
+          args: null,
+          storageKey: null,
+        },
+        {
+          kind: 'ScalarField',
+          alias: null,
+          name: 'thumbnail',
+          args: null,
+          storageKey: null,
+        },
+      ],
+    },
+  ]
+  return {
+    kind: 'Request',
+    fragment: {
+      kind: 'Fragment',
+      name: 'HomeQuery',
+      type: 'Query',
+      metadata: null,
+      argumentDefinitions: [],
+      selections: v0 /*: any*/,
+    },
+    operation: {
+      kind: 'Operation',
+      name: 'HomeQuery',
+      argumentDefinitions: [],
+      selections: v0 /*: any*/,
+    },
+    params: {
+      operationKind: 'query',
+      name: 'HomeQuery',
+      id: null,
+      text:
+        'query HomeQuery {\n  characters(where: {series: 24229}, orderBy: modified_asc) {\n    id\n    name\n    description\n    thumbnail\n  }\n}\n',
+      metadata: {},
+    },
   }
-];
-return {
-  "kind": "Request",
-  "fragment": {
-    "kind": "Fragment",
-    "name": "HomeQuery",
-    "type": "Query",
-    "metadata": null,
-    "argumentDefinitions": [],
-    "selections": (v0/*: any*/)
-  },
-  "operation": {
-    "kind": "Operation",
-    "name": "HomeQuery",
-    "argumentDefinitions": [],
-    "selections": (v0/*: any*/)
-  },
-  "params": {
-    "operationKind": "query",
-    "name": "HomeQuery",
-    "id": null,
-    "text": "query HomeQuery {\n  characters(where: {series: 24229}, orderBy: modified_asc) {\n    id\n    name\n    description\n    thumbnail\n  }\n}\n",
-    "metadata": {}
-  }
-};
-})();
-(node as any).hash = 'e3e8e5c99f716e5ac8a876df4384ccda';
-export default node;
+})()
+;(node as any).hash = 'e3e8e5c99f716e5ac8a876df4384ccda'
+export default node
