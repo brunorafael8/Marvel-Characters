809ccc67095293e6aff5ec7d28731a3ea17e78a7
brunorafael8 <bruninho_lito@hotmail.com>
2019-06-04 15:03:02 -0300

    (improve) HomeItem component

--- src/screens/Home/components/HomeItem/HomeItem.tsx
+++ src/screens/Home/components/HomeItem/HomeItem.tsx
@@ -3,17 +3,35 @@ import { createFragmentContainer, graphql, QueryRenderer } from 'react-relay'
 import styled from 'styled-components'
 import environment from '../../RelayEnvironment'
 
-const CharacterText = styled.Text``
+const getRandomColor = id => {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * id)]
+  }
+
+  return color
+}
+
+const HomeListContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${p => getRandomColor(p.id.slice(0, 2))};
+`
 const CharacterImage = styled.Image``
+const CharacterText = styled.Text``
+const CharacterDescription = styled.Text``
 
 const HomeItem = props => {
   const { character } = props
 
   return (
-    <>
-      <CharacterText>{character.name}</CharacterText>
+    <HomeListContainer id={character.id}>
       <CharacterImage style={{ width: 50, height: 50 }} source={{ uri: character.thumbnail }} />
-    </>
+      <CharacterText>{character.name}</CharacterText>
+      <CharacterDescription>{character.description}</CharacterDescription>
+    </HomeListContainer>
   )
 }
 
@@ -24,6 +42,10 @@ const HomeItemFragmentContainer = createFragmentContainer(HomeItem, {
       name
       description
       thumbnail
+      resourceURI
+      urls {
+        url
+      }
     }
   `,
 })
